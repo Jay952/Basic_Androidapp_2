41f368991cf41b9d867bcc9c3a1e299e05443fc0
[Jay952] <jaydeep4420@gmail.com>
2020-04-10 17:59:02 +0530

    my basic app 2

---
+++ Basic_app_2/app/src/main/java/com/example/basic_app_2/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.basic_app_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    public void submitnow(View view){
+        Toast.makeText(this, "Mobile Number Registered", Toast.LENGTH_SHORT).show();
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
